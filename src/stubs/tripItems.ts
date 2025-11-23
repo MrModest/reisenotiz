61e1b745fe968c7c97cfec1ffc559ea3b1ad5d5f
MrModest <babaevkamil@gmail.com>
2025-11-23 18:35:18 +0100

    add dummy stubs

---
+++ src/stubs/tripItems.ts
@@ -0,0 +1,52 @@
+import { trips } from './trips'
+
+const persons = [
+  { id: 'vvc32db4-d180-46a1-bc30-afa9dccc85b3', fullname: 'Max Mustermann', contacts: [] },
+  { id: '8325281d-1901-4eda-9c4e-58f58b94893d', fullname: 'Maria Mustermann', contacts: [] },
+  { id: 'f12afa65-22c8-4545-8f34-0523d00c789d', fullname: 'Mark Smith', contacts: [] }
+]
+
+const airports = {
+  "BER": {
+    code: "BER",
+    name: "Berlin Brandenburg Airport",
+    city: "Berlin",
+    country: "Germany",
+    tzone: "Europe/Berlin"
+  },
+  "HND": {
+    code: "HND",
+    name: "Tokyo Haneda Airport",
+    city: "Tokyo",
+    country: "Japan",
+    tzone: "Asia/Tokyo"
+  },
+}
+
+export const tripsItems = [{
+  id: "bbc32db4-d180-41a1-bc30-afa9dccc85b3",
+  tripId: trips[0].id,
+  title: 'Flight to Tokyo',
+  type: 'Flight',
+  flightNumber: 'PC5030',
+  carrier: 'Pegasus',
+  bookingCode: 'Q4LTAL',
+  seat: '12F, 36B',
+  passengers: persons,
+  departure: {
+    airport: airports["BER"],
+    terminal: '2',
+    gate: 'A24',
+    time: { date: '2023-04-01T18:00:00.000Z', zone: airports["BER"].tzone }
+  },
+  arrival: {
+    airport: airports["HND"],
+    terminal: '1',
+    gate: 'unknown',
+    time: { date: '2023-04-02T10:00:00.000Z', zone: airports["HND"].tzone }
+  },
+  note: 'Empty note',
+  attachments: []
+}]
+
+
--- src/stubs/trips.ts
+++ src/stubs/trips.ts
@@ -1,11 +1,11 @@
 export const trips = [{
-  id: "1",
+  id: "ffc32db4-d185-41a1-bc30-rcwecwc85b3",
   title: 'Trip to Japan',
   startDate: '2023-04-01',
   endDate: '2023-04-15',
   description: 'Exploring Tokyo, Kyoto, and Osaka.',
 }, {
-  id: "2",
+  id: "abb12db4-d180-41a1-bc30-afa9dccc85b4",
   title: 'European Tour',
   startDate: '2023-06-10',
   endDate: '2023-06-30',
