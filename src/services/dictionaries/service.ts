f47f450712b1d1be9409ac62711b691ae907e479
MrModest <babaevkamil@gmail.com>
2025-12-28 22:42:21 +0100

    update dict service

--- src/services/dictionaries/service.ts
+++ src/services/dictionaries/service.ts
@@ -2,10 +2,13 @@ import { DictionaryConfig, StoredDict } from "./types"
 
 export class Dictionary<T extends { name: string }> {
   private cache: Record<string, T> = {}
+  private fetchedAt: string = ''
   private loaded = false
 
   constructor(private readonly config: DictionaryConfig<T>) { }
 
+  // FIXME: Current logic will lose any custom values stored in localStorage.
+  // Need a way for a proper merging public dataset with user's custom entries.
   async load(): Promise<void> {
     if (this.loaded) return
 
@@ -19,6 +22,7 @@ export class Dictionary<T extends { name: string }> {
 
         if (Number.isFinite(fetchedAt) && now - fetchedAt <= this.config.maxAgeMs) {
           this.cache = stored.data
+          this.fetchedAt = stored.fetchedAt
           this.loaded = true
           return
         }
@@ -62,6 +66,23 @@ export class Dictionary<T extends { name: string }> {
   getAll(): Readonly<Record<string, T>> {
     this.ensureLoaded()
     return this.cache
+  }
+
+  async add(item: T): Promise<void> {
+    this.ensureLoaded()
+    this.cache[item.name] = item
+
+    const payload: StoredDict<T> = {
+      fetchedAt: this.fetchedAt, // don't overwrite so server updates can be detected
+      data: this.cache
+    }
+
+    localStorage.setItem(
+      this.config.storageKey,
+      JSON.stringify(payload)
+    )
+
+    //TODO: add the new item to server via API
   }
 
   findByName(name: string): T[] {
