8a8a437d64aedee968082179fc73b497ded713c1
MrModest <babaevkamil@gmail.com>
2025-12-30 14:37:12 +0100

    Extract airport selector into a separate component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,41 +1,13 @@
 import { useState } from 'react'
 import { Button } from '@/components/ui/button'
-import { airports } from '@/stubs/airports'
-import { Combobox, ComboboxOptions } from '@/components/ui/combobox'
 import { Separator } from '@/components/ui/separator'
+import { AirportCombobox, AirportOption } from '@/components/trip-item/flight/airport/airport-combobox'
+import { airportDictionary } from '@/services'
+import { AddressNone } from '@/types/trip/address'
 
 export function HomePage() {
   const [count, setCount] = useState(0)
-
-  const [airportOptions, setAirportOptions] = useState<ComboboxOptions[]>(
-    airports.map((airport) => ({
-      value: airport.code,
-      label: airport.name
-    }))
-  )
-
-  function addAirportOption(option: ComboboxOptions) {
-    setAirportOptions((prevOptions) => [...prevOptions, option])
-  }
-
-  const [selectedAirport, setSelectedAirport] = useState<string | null>(null)
-
-    function handleSelect(option: ComboboxOptions) {
-      console.log('handleSelect')
-      console.log(option)
-      setSelectedAirport(option.value)
-    }
-
-    function handleAppendGroup(label: ComboboxOptions['label']) {
-      const newAirport = {
-        value: label.slice(0, 3).toUpperCase(),
-        label: label
-      }
-      addAirportOption(newAirport)
-      console.log('handleAppendGroup')
-      console.log(newAirport)
-      handleSelect(newAirport)
-    }
+  const [selectedAirport, setSelectedAirport] = useState<AirportOption | null>(null)
 
   return (
     <div className='min-h-screen flex flex-col items-center p-4'>
@@ -43,13 +15,24 @@ export function HomePage() {
         Click Me ({count})
       </Button>
       <Separator className='my-4 w-full' orientation='horizontal' />
-      <Combobox
-        options={airportOptions}
-        placeholder="Select airport"
-        selected={selectedAirport ?? ''}
-        onChange={handleSelect}
-        onCreate={handleAppendGroup}
+      <AirportCombobox
+        options={airportDictionary.getAllValues().map((airport) => ({
+          code: airport.code,
+          name: airport.name
+        }))}
+        value={selectedAirport}
+        onSelect={setSelectedAirport}
+        onCreate={handleCreateAirport}
       />
     </div>
   )
+}
+
+function handleCreateAirport(newAirport: AirportOption) {
+  airportDictionary.add({
+    code: newAirport.code,
+    name: newAirport.name,
+    address: AddressNone,
+    tzone: 'UTC'
+  })
 }
