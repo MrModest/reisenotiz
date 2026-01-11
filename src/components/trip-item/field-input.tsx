0a6dc4954143ebe885979f394d1a4b70104564f6
MrModest <babaevkamil@gmail.com>
2026-01-11 16:40:11 +0100

    fix validation

--- src/components/trip-item/field-input.tsx
+++ src/components/trip-item/field-input.tsx
@@ -1,7 +1,7 @@
-import { Field, FieldLabel } from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
-import { cn } from "@/lib/utils"
-import { useFormContext } from "react-hook-form"
+import { Field, FieldError, FieldLabel } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import { cn } from '@/lib/utils'
+import { useController, useFormContext } from 'react-hook-form'
 
 interface FieldInputProps {
   name: string
@@ -10,15 +10,14 @@ interface FieldInputProps {
 };
 
 export function FieldInput({ name, label, className }: FieldInputProps) {
-  const { register, formState: { errors }, } = useFormContext()
-
-  const error = errors[name]?.message as string | undefined
+  const { control } = useFormContext()
+  const { field, fieldState: { error } } = useController({ name, control })
 
   return (
     <Field className={cn('gap-0', className)}>
       <FieldLabel htmlFor={name}>{label}</FieldLabel>
-      <Input className='h-fit px-2 rounded-sm' id={name} {...register(name)} />
-      {error && <span className='text-xs font-thin text-foreground'>{error}</span>}
+      <Input aria-invalid={!!error} className='h-fit px-2 rounded-sm' id={name} {...field} />
+      {error && <FieldError className='text-xs font-thin text-foreground'>{error.message?.toString()}</FieldError>}
     </Field>
   )
 }
