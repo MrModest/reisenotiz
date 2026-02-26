78e136f72d2158d6eae978e57c493118a9d5c1b6
MrModest <babaevkamil@gmail.com>
2026-02-26 16:30:21 +0100

    adapt `FieldInput` to work with numbers

--- src/components/trip-item/field-input.tsx
+++ src/components/trip-item/field-input.tsx
@@ -17,13 +17,19 @@ interface FieldInputProps {
 export function FieldInput({ name, label, required: isRequired = false, disabled, placeholder, className, type }: FieldInputProps) {
   const { field, error } = useFormField(name)
 
+  const isNumber = type === 'number'
+  const onChange = isNumber
+    ? (e: React.ChangeEvent<HTMLInputElement>) => field.onChange(e.target.valueAsNumber)
+    : field.onChange
+  const value = isNumber && Number.isNaN(field.value) ? '' : field.value
+
   return (
     <Field className={cn('gap-0.5', className)}>
       <FieldLabel className='gap-1 pr-10' htmlFor={name}>
         {label}
         {isRequired && (<Required />)}
       </FieldLabel>
-      <Input aria-invalid={!!error} id={name} type={type as string} disabled={disabled} placeholder={placeholder} {...field} />
+      <Input aria-invalid={!!error} id={name} type={type as string} disabled={disabled} placeholder={placeholder} {...field} onChange={onChange} value={value} />
       {error && <FieldError className='text-xs font-thin'>{error.message?.toString()}</FieldError>}
     </Field>
   )
